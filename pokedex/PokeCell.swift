c9db71a23690b700d328d0e6f1f27bc2cc2fab58
Ryan New <ryannew@Ryans-MacBook-Pro.local>
2017-03-31 17:02:23 +0100

    sorted view controller

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,22 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Ryan New on 31/03/2017.
+//  Copyright © 2017 Ryan New. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
